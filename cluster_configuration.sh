488be142b6a7b4b21443032811887ff5e6be7a09
valhallaTamish <79277219+valhallaTamish@users.noreply.github.com>
2021-07-18 14:47:34 +0530

    Create cluster_configuration.sh

---
+++ cluster_configuration.sh
@@ -0,0 +1,4 @@
+echo "Installing Kubectl....."
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.18.0/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
